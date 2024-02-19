df6b725ff13f1e995047bc9cea918ea4c79dad9d
Arun Kumar <aruncode49@gmail.com>
2024-02-19 15:36:57 +0530

    implement express session and created user model with plm

--- server/index.js
+++ server/index.js
@@ -1,17 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import passport from "passport";
+import localStrategy from "passport-local";
+import session from "express-session";
 
 import { connectDB } from "./db/connect.js";
+import { User } from "./models/user.model.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = 8080;
 
+// session options
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sessionOptions));
 
 app.get("/", (req, res) => {
   res.send("Hello From Codify Server");
