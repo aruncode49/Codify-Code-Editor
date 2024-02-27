3f1a79f295590a58a5f35e93fe9da349462b1a88
Arun Kumar <aruncode49@gmail.com>
2024-02-27 15:53:58 +0530

    adding initial code to redux

--- client/src/app/code/codeSlice.js
+++ client/src/app/code/codeSlice.js
@@ -2,9 +2,53 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   fullCode: {
-    html: "",
-    css: "",
-    javascript: "",
+    html: `<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Basic Example</title>
+<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+
+<div class="container">
+  <button id="myButton">Click me!</button>
+  <p id="output"></p>
+</div>
+
+<script src="script.js"></script>
+
+</body>
+</html>
+    `,
+    css: `body {
+  font-family: Arial, sans-serif;
+}
+
+.container {
+  text-align: center;
+  margin-top: 100px;
+}
+
+button{
+  padding: 1rem 2rem;
+  font-size: 2rem;
+  border-radius: 1rem;
+  background: lightblue;
+  cursor: pointer;
+  margin-top: 100px;
+  
+}
+
+#output {
+  font-size: 24px;
+  color: blue;
+}
+    `,
+    javascript: `document.getElementById("myButton").addEventListener("click", function() {
+  document.getElementById("output").innerText = "Hello, world!";
+});
+    `,
   },
   currentLanguage: "html",
 };
